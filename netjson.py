07fcca57aa11aa80161b59296dec9f0c7f7ab8af
William Zhang <willzhang05@gmail.com>
2018-07-10 00:20:45 -0400

    add link reporting

--- netjson.py
+++ netjson.py
@@ -29,16 +29,10 @@ def get_interface_ip_addresses(dev, ifl, primary_only=False):
                 primary_res = primary_find(m)
                 if len(primary_res) == 0:
                     continue
-            dest_find = etree.XPath("ifa-destination")
-            dest_res = dest_find(m)
-
             ifa_find = etree.XPath("ifa-local")
             ifa_res = ifa_find(m)
             if len(ifa_res) != 0:
-                if len(dest_res) == 0:
-                    addr.append(ifa_res[0].text.strip() + "/32")
-                else:
-                    addr.append(dest_res[0].text.strip())
+                addr.append(ifa_res[0].text.strip())
     return addr
 
 
@@ -46,38 +40,70 @@ def get_node_info(dev):
     ifaces = []
     addresses = []   
     output = dev.rpc.get_lldp_local_info()
+    mgmt_if_find = etree.XPath("//lldp-local-management-address-interface-name")
+    mgmt_if = mgmt_if_find(output)
+    mgmt_if_name = ""
+    if len(mgmt_if) != 0:
+        mgmt_if_name = mgmt_if[0].text.strip()
+
     if_find = etree.XPath("//lldp-local-interface-name")
     lldp_if = if_find(output)
     if len(lldp_if) != 0:
         for iface in lldp_if:
-            ifaces.append(iface.text.strip())
+            iface_name = iface.text.strip()
+            if iface_name != mgmt_if_name:
+                ifaces.append(iface.text.strip())
+
     for i in ifaces:
         addr = get_interface_ip_addresses(dev, i, primary_only=True)
         addresses += addr
 
+    mgmt_addr_find = etree.XPath("//lldp-local-management-address-address")
+    mgmt_addr = mgmt_addr_find(output)[0].text.strip()
+    
     hostname_find = etree.XPath("//lldp-local-system-name")
     hostname = hostname_find(output)[0].text.strip()
 
     id_find = etree.XPath("//lldp-local-chassis-id")
-    host_id = str(hostname + ''.join(id_find(output)[0].text.split(":")))
+    chassis_id = id_find(output)[0].text
+    host_id = str(hostname + ''.join(chassis_id.split(":")))
 
     hex_host_id = bytes(host_id.ljust(16)[:16])
     host_id = uuid.UUID(bytes=hex_host_id)
 
     data = {}
-    data['id'] = str(host_id)
-    data['label'] = hostname
+    data['id'] = chassis_id
+    data['label'] = hostname + " - " + mgmt_addr
+    data['properties'] = {}
+    data['properties']['hostname'] = hostname
+    data['properties']['address'] = mgmt_addr
     data['local_addresses'] = addresses
     return (ifaces,data)
+
+
+def get_link_info(dev, node_data):
+    output = dev.rpc.get_lldp_neighbors_information()
+    neighbor_find = etree.XPath("//lldp-remote-chassis-id")
+    neighbors = neighbor_find(output)
+
+    data = []
+    for neighbor in neighbors:
+        entry = {}
+        entry['source'] = node_data['id']
+        entry['target'] = neighbor.text.strip()
+        data.append(entry)
+    
+    return data
+
 
 def main():
     dev = Device()
     dev.open()
 
     node_ifaces, node_data = get_node_info(dev)
-    print(node_ifaces)
     print(node_data)
-    
+    link_data = get_link_info(dev, node_data)
+    print(link_data)
     dev.close()
     return
 
